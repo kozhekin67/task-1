9d928557983e6acf1316851a48d6da14a44a925a
kozhekin67 <kozhekin.alexei@yandex.ru>
2023-12-03 15:11:13 +0300

    renaming classes, fixed styles for card-tour and travel-history blocks, rewriting viewports with the addition of mixins for adaptivity, putting colors in a separate file, renaming images and sorting by folders

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-document.querySelector('.dropdown_button').addEventListener('click', function () {
-    document.querySelector('.dropdown_list').classList.toggle('dropdown_list--visible');
+document.querySelector('.dropdown__button').addEventListener('click', function () {
+    document.querySelector('.dropdown__list').classList.toggle('dropdown__list_visible');
 });
 
-document.querySelectorAll('.dropdown_list-item').forEach(function(listItem) {
+document.querySelectorAll('.dropdown__list-item').forEach(function(listItem) {
     listItem.addEventListener('click', function (event) {
         event.stopPropagation ();
-        document.querySelector('.dropdown_button').innerText = this.innerText;
-        document.querySelector('.dropdown_button').classList.add('dropdown_button--active');
-        document.querySelector('.dropdown_list').classList.remove('dropdown_list--visible');
+        document.querySelector('.dropdown__button').innerText = this.innerText;
+        document.querySelector('.dropdown__button').classList.add('dropdown__button_active');
+        document.querySelector('.dropdown__list').classList.remove('dropdown__list_visible');
     })
 });
 
 document.addEventListener('click', function (event){
-    if (event.target !== document.querySelector('.dropdown_button')) {
-        document.querySelector('.dropdown_list').classList.remove('dropdown_list--visible');
+    if (event.target !== document.querySelector('.dropdown__button')) {
+        document.querySelector('.dropdown__list').classList.remove('dropdown__list_visible');
     }
 });
 
 document.addEventListener('click', function(event) {
-    let refund = document.querySelector('.refund');
+    let refund = document.querySelector('.dropdown__refund');
     if (event.target == refund) {
-        document.querySelector('.dropdown_button').innerText = innerText = 'Куда хотите ехать';
-        document.querySelector('.dropdown_button').classList.remove('dropdown_button--active');
+        document.querySelector('.dropdown__button').innerText = innerText = 'Куда хотите ехать';
+        document.querySelector('.dropdown__button').classList.remove('dropdown__button_active');
     }
 });
 
 window.onscroll = function showMenu () {
-    let menu = document.querySelector('.menu');
-    let links = document.querySelectorAll('.nav-link');
-    let linksHover = document.querySelectorAll('.nav-link');
-    let logo = document.querySelector('.header-logo');
-    let telephone = document.querySelector('.phone-number');
+    let menu = document.querySelector('.header-block__menu');
+    let links = document.querySelectorAll('.site-sections__section-link');
+    let linksHover = document.querySelectorAll('.site-sections__section-link');
+    let logo = document.querySelector('.your-tur__img');
+    let telephone = document.querySelector('.phone-number__reference-number');
 
 
     if (window.pageYOffset > 450) {
-        menu.classList.add('menu_scroll');
+        menu.classList.add('header-block__menu_scroll');
         links.forEach((link)=>link.style.color = '#1B1F2B');
-        linksHover.forEach((l)=>l.classList.add('nav-link__scrool'));
-        logo.classList.add('header-logo__scrool');
-        telephone.classList.add('phone-number__scrool');
+        linksHover.forEach((l)=>l.classList.add('site-sections__section-link_scrool'));
+        logo.classList.add('your-tur__img_scrool');
+        telephone.classList.add('phone-number__reference-number_scrool');
     }
     
     else {
-        menu.classList.remove('menu_scroll');
+        menu.classList.remove('header-block__menu_scroll');
         links.forEach((link)=>link.style.color = 'white');
-        linksHover.forEach((l)=>l.classList.remove('nav-link__scrool'));
-        logo.classList.remove('header-logo__scrool');
-        telephone.classList.remove('phone-number__scrool');
+        linksHover.forEach((l)=>l.classList.remove('site-sections__section-link_scrool'));
+        logo.classList.remove('your-tur__img_scrool');
+        telephone.classList.remove('phone-number__reference-number_scrool');
     }
-    
 }
 
 const sections = document.querySelectorAll('a[href*="#"]')
